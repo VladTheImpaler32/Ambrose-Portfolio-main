546ab9da7eae8f9bd8c36996650ed6ba6137e5c3
VladTheImpaler32 <esorbma.nosilla@icloud.com>
2022-08-12 14:21:56 -0700

    new file:   about.html 	new file:   images/IMG_0094-Edit.jpg     new file:   images/IMG_0375.jpg 	new file:   images/IMG_0600.jpg 	new file:   images/IMG_0634-Edit.jpg 	new file:   images/IMG_0795.jpg 	new file:   images/IMG_0813.jpg 	new file:   images/IMG_0816.jpg 	new file:   images/IMG_0844.jpg 	new file:   images/IMG_0874.jpg 	new file:   images/IMG_0900.jpg 	new file:   images/IMG_1042-Edit-2.jpg 	new file:   images/IMG_7624.jpg 	new file:   images/IMG_7800.jpg 	new file:   images/IMG_8086.jpg 	new file:   images/IMG_8111.jpg 	new file:   images/IMG_8124.jpg 	new file:   images/IMG_8331.jpg 	new file:   images/IMG_8549.jpg 	new file:   images/IMG_9272-Edit.jpg 	new file:   images/IMG_9323.jpg 	new file:   images/IMG_9328-Edit.jpg 	new file:   images/IMG_9574.jpg 	new file:   images/IMG_9801-Edit.jpg 	new file:   index.html 	new file:   notes.html 	new file:   other.html 	new file:   other/experiments.html 	new file:   other/experiments/html.html 	new file:   other/experiments/html/sort.html     new file:   other/experiments/html/spy.html 	new file:   other/experiments/other.html 	new file:   other/experiments/python.html 	new file:   other/form.html 	new file:   other/ico/folder-1.png 	new file:   other/ico/folder-2.png 	new file:   other/ico/folder-3.png 	new file:   other/ico/folder-4.png 	new file:   other/ico/folder-5.png 	new file:   other/ico/folder-6.png 	new file:   other/ico/folder-7.png 	new file:   other/ico/folder-8.png 	new file:   other/page.php 	new file:   photos.html

---
+++ other/page.php
@@ -0,0 +1,24 @@
+<?php
+if ($_POST['pass'] == 'bob')
+echo 'Welcome to the protected page!';
+
+else
+header('Location:form.html');
+?>
+<html>
+<HEAD>
+<TITLE>ambrose allison</TITLE>
+<link rel="stylesheet" href="main1.css" media="screen" /> <link rel="stylesheet" href="mobile1.css" media="screen and (max-device-width: 800px)" />
+ <media="screen and (max-device-width: 800px)" />
+<meta name="description" content="other">
+</HEAD>
+<meta name="viewport" content="width=device-width, initial-scale=0.7,"/>
+
+<font style="font-family:times;font-size:56px;">
+<body style="word-wrap: initial; background-color: black">
+
+&nbsp;<p style="text-decoration:none; font-size:56px;"><font color=#EA638C>&nbsp;&nbsp;&nbsp;you made it!</font></p>
+&nbsp;<p style="text-decoration:none; font-size:36px;"><font color=#30343F>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;now what?</font></p>
+</body>
+
+</html>
